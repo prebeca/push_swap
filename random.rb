d41645d889ed5b77144d61ae5508f2b87008b40d
Pierre <58135780+prebeca@users.noreply.github.com>
2021-08-21 00:52:08 +0200

    Typo
    Typo in executable name in error messages

--- random.rb
+++ random.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/ruby
 
 if ARGV.length != 3
-	puts "random.ruby: wrong number of arguments, requires 3 integers \n\t- usage: random.ruby [min value] [max value] [number of values]"
+	puts "random.rb: wrong number of arguments, requires 3 integers \n\t- usage: random.rb [min value] [max value] [number of values]"
 	exit 1
 end
 
 if	ARGV[0].to_i >= ARGV[1].to_i
-	puts "random.ruby: min value must be less than max value"
+	puts "random.rb: min value must be less than max value"
 	exit 1
 end
 
 if	ARGV[2].to_i <= 0
-	puts "random.ruby: number of values muast be greater than 0"
+	puts "random.rb: number of values must be greater than 0"
 	exit 1
 end
 
